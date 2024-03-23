adb3d60c7bd0626e0bdf6c0a7b118d6fe36ff8e8
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2024-03-23 20:58:55 +0000

    registedSFS

---
+++ src/utils/SFSreg.sol
@@ -0,0 +1,20 @@
+// // SPDX-License-Identifier: GPL-3.0
+// pragma solidity ^0.8.20;
+
+
+// interface ISFS {
+//     function register(address _recipient) external returns (uint256 tokenId);
+// }
+
+// contract ModeSFSToken {
+
+//     address feeReceiver = msg.sender;
+    
+//     constructor()  {
+//         feeReceiver = msg.sender; //The deployer of the contract will get the NFT to widthraw the earned fees
+//         ISFS sfsContract = ISFS(0x8680CEaBcb9b56913c519c069Add6Bc3494B7020); // This address is the address of the SFS contract
+//         sfsContract.register(msg.sender); //Registers this contract and assigns the NFT to the owner of this contract
+//     }
+// }
+
+// contractaddress: 0x7451e103E206157a1E495A9aE8CcC1753522D3bc
